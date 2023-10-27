1d6e81a8d1ae58de09efe37452a306139bbe9ad2
t-montes <t.montes@uniandes.edu.co>
2023-10-27 11:51:15 -0500

    fix prettier error and set morgan logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,8 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.use(morgan('tiny'));
   app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
 }
 bootstrap();
