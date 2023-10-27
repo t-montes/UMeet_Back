ea39430596186ee955fe13b6267c9657c3dee184
t-montes <t.montes@uniandes.edu.co>
2023-10-27 12:46:42 -0500

    users persistence, logic and API

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableVersioning({
-    type: VersioningType.URI,
-    prefix: 'api/v',
-    defaultVersion: '1',
-  });
+    const app = await NestFactory.create(AppModule);
+    app.enableVersioning({
+        type: VersioningType.URI,
+        prefix: 'api/v',
+        defaultVersion: '1',
+    });
 
-  app.use(morgan('tiny'));
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+    app.use(morgan('tiny'));
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(3001);
 }
 bootstrap();
---
+++ src/user/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+    ) {}
+
+    async findAll(): Promise<UserEntity[]> {
+        return await this.userRepository.find(/*{relations: ['calendar', 'friends', 'groups']}*/);
+    }
+
+    async findOne(id: string): Promise<UserEntity> {
+        const user: UserEntity = await this.userRepository.findOne({
+            where: { id },
+            relations: ['calendar', 'friends', 'groups'],
+        });
+        if (!user) throw new Error('The user with the given id was not found');
+        return user;
+    }
+
+    async create(user: UserEntity): Promise<UserEntity> {
+        return await this.userRepository.save(user);
+    }
+
+    async update(id: string, user: UserEntity): Promise<UserEntity> {
+        const persistedUser: UserEntity = await this.userRepository.findOne({
+            where: { id },
+        });
+        if (!persistedUser)
+            throw new Error('The user with the given id was not found');
+
+        user.id = id;
+
+        return await this.userRepository.save(user);
+    }
+
+    async delete(id: string) {
+        const user: UserEntity = await this.userRepository.findOne({
+            where: { id },
+        });
+        if (!user) throw new Error('The user with the given id was not found');
+
+        await this.userRepository.remove(user);
+    }
+}
