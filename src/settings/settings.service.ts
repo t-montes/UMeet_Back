508cc79e9f5ed5effdaf3cbc5ba03d5cddbcef01
t-montes <t.montes@uniandes.edu.co>
2023-11-28 08:57:31 -0500

    rebase remove import

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SettingsEntity } from './settings.entity';
 import { UserEntity } from '../user/user.entity';
-import { validateEntity } from '../shared/utils';
 
 @Injectable()
 export class SettingsService {
