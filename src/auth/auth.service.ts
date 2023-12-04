d5f0a86d56ab62042ca74bb6e7194ee147a1a25a
t-montes <t.montes@uniandes.edu.co>
2023-12-03 21:35:54 -0500

    fix prettier

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,25 +6,33 @@ import { PersonService } from '../person/person.service';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: PersonService,
-        private jwtService: JwtService
-    ) {}
+  constructor(
+    private usersService: PersonService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(username: string, password: string): Promise<any> {
-        const user: APIUser = await this.usersService.findOne(username);
-        if (user && user.password === password) {
-            const { password, permissions, ...result } = user;
-            return {...result, permissions};
-        }
-        return null;
+  async validateUser(username: string, password: string): Promise<any> {
+    const user: APIUser = await this.usersService.findOne(username);
+    if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, permissions, ...result } = user;
+      return { ...result, permissions };
     }
+    return null;
+  }
 
-    async login(req: any) {
-        const user: APIUser = await this.usersService.findOne(req.user.username);
-        const payload = { username: req.user.username, sub: req.user.id,  permissions: user.permissions};
-        return {
-            token: this.jwtService.sign(payload, { privateKey: constants.JWT_SECRET, expiresIn:constants.JWT_EXPIRES_IN }),
-        };
-    }
+  async login(req: any) {
+    const user: APIUser = await this.usersService.findOne(req.user.username);
+    const payload = {
+      username: req.user.username,
+      sub: req.user.id,
+      permissions: user.permissions,
+    };
+    return {
+      token: this.jwtService.sign(payload, {
+        privateKey: constants.JWT_SECRET,
+        expiresIn: constants.JWT_EXPIRES_IN,
+      }),
+    };
+  }
 }
