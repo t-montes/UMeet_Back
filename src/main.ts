02eaab4a5e72a565440a1469d463b6034dfd3eaa
t-montes <t.montes@uniandes.edu.co>
2023-12-04 09:34:53 -0500

    accept requests from React front-end

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'api/v',
     defaultVersion: '1',
   });
-
   app.use(morgan('tiny'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
