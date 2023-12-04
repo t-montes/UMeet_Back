5d8ba0c87171195886fca52fa8a84fbcc83641bf
SnowArtz <ds.ortiz@uniandes.edu.co>
2023-12-03 21:19:43 -0500

    What?

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import constants from '../shared/security/constants';
@@ -15,19 +14,17 @@ export class AuthService {
     async validateUser(username: string, password: string): Promise<any> {
         const user: APIUser = await this.usersService.findOne(username);
         if (user && user.password === password) {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { password, permissions, ...result } = user;
-            return { ...result, permissions };
+            return {...result, permissions};
         }
         return null;
     }
 
     async login(req: any) {
         const user: APIUser = await this.usersService.findOne(req.user.username);
-        const payload = { username: req.user.username, sub: req.user.id,  permissions: user.permissions };
+        const payload = { username: req.user.username, sub: req.user.id,  permissions: user.permissions};
         return {
             token: this.jwtService.sign(payload, { privateKey: constants.JWT_SECRET, expiresIn:constants.JWT_EXPIRES_IN }),
         };
     }
-
 }
